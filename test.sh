e069df15c5615689f4dbf7a3b42a8d1ba6e8f14e
michaelJava69 <michael.professional@inbox.com>
2019-10-08 17:34:21 +0100

    hello

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-hi michael  "aws-0-0-0-0--0"  i am having a nightmare day  "a-am-having-0-0-0-0--0"   day you know 0-0-0-0--0
+hi michael  "0-0-0-0--0"  i am having a nightmare day  "a-am-having-0-0-0-0--0"   day you know 0-0-0-0--0
